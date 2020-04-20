85ea11826e5d8611a0541d7297385859ca945e83
Mads Knudsen <madsfknudsen@hotmail.com>
2020-04-20 12:55:44 -1100

    +

---
+++ Teknikfags Eksamen/Assets/_Scripts/World/ObjectReset.cs
@@ -0,0 +1,40 @@
+﻿#region Systems
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class ObjectReset : MonoBehaviour
+{
+    public List<GameObject> Objects = new List<GameObject>();
+    List<Transform> Transforms = new List<Transform>();
+
+    void Start()
+    {
+        foreach (GameObject O in Objects)
+        {
+            Transforms.Add(O.transform);
+        }
+    }
+
+    public void ResetObjectTransform()
+    {
+        for (int i = 0; i < Objects.Count; i++)
+        {
+            Objects[i].transform.position = Transforms[i].position;
+            Objects[i].transform.rotation = Transforms[i].rotation;
+        }
+    }
+
+    void OnTriggerExit(Collider O)
+    {
+        for (int i = 0; i < Objects.Count; i++)
+        {
+            if (Objects[i] == O)
+            {
+                Objects[i].transform.position = Transforms[i].position;
+                Objects[i].transform.rotation = Transforms[i].rotation;
+            }
+        }
+    }
+}
